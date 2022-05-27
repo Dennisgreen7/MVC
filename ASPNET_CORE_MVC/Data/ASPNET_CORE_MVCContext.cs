6a92032eabe757471821bb4c9e1fe26bfdd49155
Dennisgreen7 <denis4167@gmail.com>
2022-05-27 20:11:42 +0300

    first commit

---
+++ ASPNET_CORE_MVC/Data/ASPNET_CORE_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPNET_CORE_MVC.Models;
+
+namespace ASPNET_CORE_MVC.Data
+{
+    public class ASPNET_CORE_MVCContext : DbContext
+    {
+        public ASPNET_CORE_MVCContext (DbContextOptions<ASPNET_CORE_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNET_CORE_MVC.Models.Recipe> Recipe { get; set; }
+    }
+}
---
+++ ASPNET_CORE_MVC/Models/Recipe.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNET_CORE_MVC.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string CakeNmae { get; set; }
+        public string CakeRecipe { get; set; }
+    }
+}
